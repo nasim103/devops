3e8263f6382ca4a2b45a22586529f650cdf67e31
nasim103 <nasim1039@gmail.com>
2024-03-30 16:00:44 +0530

    Update cis_audit.sh

--- oracle-linux-8/scripts/cis_audit.sh
+++ oracle-linux-8/scripts/cis_audit.sh
@@ -1,5 +1,5 @@
 export GIT_SSL_NO_VERIFY=true
 cd /opt/
-git clone --depth 1 -b main https://packer:glpat-RKCWeE_o9M13t6TQ4diX@gitlab.css.cgipdc.cginet/unix-iac/cis/public/harden_audit.git
+git clone --depth 1 -b main 
 mv harden_audit_rhel8 RHEL8-CIS-Audit
 chmod +x /opt/RHEL8-CIS-Audit/run_audit.sh
